cec10526fd9a6ec2dae27eac8560e16bb208aa65
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2022-12-08 18:17:24 +0700

    feat: add esm exclusive dist folder

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,23 @@
+import type { Options } from 'tsup';
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-	entry: ['lib/index.ts'],
-	format: ['cjs', 'esm'],
+const commonOptions: Options = {
+	entry: ['lib/index.ts', 'lib/user/*.ts'],
 	clean: true,
 	dts: true,
-});
+};
+
+const buildOptions: Options[] = [
+	{
+		...commonOptions,
+		format: ['cjs'],
+		outDir: 'dist',
+	},
+	{
+		...commonOptions,
+		format: ['esm'],
+		outDir: 'dist/esm',
+	},
+];
+
+export default defineConfig(buildOptions);
